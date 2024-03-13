bbf4b1a8b4b76cee374f2c197d264f27e95edf75
Ralf-A <ralfanimagi@gmail.com>
2024-03-13 12:42:57 +0200

    Model for btc_wallet_transaction

--- btc_wallet/models.py
+++ btc_wallet/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Transaction(models.Model):
+    # A random unique hexadecimal string
+    transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Amount in BTC, with max_digits set to accommodate the largest possible BTC value and decimal_places for precision
+    amount = models.DecimalField(max_digits=19, decimal_places=10)
+    # Whether this transaction has been used in a money transfer
+    spent = models.BooleanField(default=False)
+    # The timestamp when this transaction was created
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transaction {self.transaction_id}"
